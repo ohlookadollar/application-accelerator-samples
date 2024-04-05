df18c22f16d16d17b38407095231e85e3ac62f19
Raymond Lee <rlee@pivotal.io>
2024-04-05 15:53:45 -0400

    crash on start up

--- tanzu-java-web-app/src/main/java/com/example/springboot/Application.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/Application.java
@@ -13,8 +13,8 @@ import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	public static void main(String[] args) throws Exception {
+		throw new Exception();
 	}
 
 	@Bean
